a29caae4132847b117e4743d175fb855dcccc2fe
KwonL <dlrnjsgud322@gmail.com>
2018-12-03 02:43:16 +0900

    Upload project 4

---
+++ project4/skeleton_proj4/scope.c
@@ -0,0 +1,186 @@
+#include <stdio.h>
+#include <strings.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "subc.h"
+
+void push_scope() {
+    // printf("%d: push scope\n", read_line());
+    struct stack_node* cur_node = (struct stack_node *)malloc(sizeof(struct stack_node));
+
+    cur_node->next = NULL;
+    cur_node->prev = top;
+    if (top != NULL) {
+        cur_node->ste = top->ste;
+    } else {
+        cur_node->ste = NULL;
+    }
+
+    top = cur_node;
+
+    return;
+}
+
+void insert(struct id* arg_id, struct decl* arg_decl) {
+    if (arg_id == NULL || arg_decl == NULL) 
+        return;
+    struct ste* node = (struct ste *)malloc(sizeof(struct ste));
+
+    // initialize new node
+    node->prev = top->ste;
+    top->ste = node;
+    node->decl = arg_decl;
+    node->name = arg_id;
+
+    return;
+}
+
+struct ste* pop_scope() {
+    // printf("%d: pop scope\n", read_line());
+    if (top == NULL) return NULL;
+    struct ste* cur_node = top->ste;
+    struct ste* ste_top = top->ste;
+
+    // There was no insertion on this scope
+    if (ste_top == top->prev->ste) {
+        // Free scope stack top
+        struct stack_node* prev_node = top->prev;
+        prev_node->next = NULL;
+        free(top);
+        top = prev_node;
+
+        return NULL;
+    }
+    // detach nodes from stack
+    // printf("%d: top->prev is : %p\n", read_line(), top->prev);
+    while (cur_node->prev != top->prev->ste) {
+        // printf("%d: cur_node is : %s\n", read_line(), cur_node->name->name);
+        cur_node = cur_node->prev;
+    }
+    cur_node->prev = NULL;
+    // printf("checking\n");
+    
+
+    // Reverse list
+    // cur_node point to end of this scope's ste
+    struct ste* prev   = NULL; 
+    struct ste* current = top->ste; 
+    struct ste* next = NULL; 
+    struct ste* ret = NULL;
+    while (current != NULL) { 
+        // Store next 
+        next  = current->prev;   
+  
+        // Reverse current node's pointer 
+        current->prev = prev;    
+  
+        // Move pointers one position ahead. 
+        prev = current; 
+        current = next; 
+    } 
+    ret = prev;
+
+    // Free scope stack top
+    struct stack_node* prev_stack = top->prev;
+    prev_stack->next = NULL;
+    free(top);
+    top = prev_stack;
+
+    /* for debug */
+    current = ret; 
+    while (current != NULL) {
+        // printf("%d: Now node is %s\n", read_line(), current->name->name);
+        current = current->prev;
+    }
+
+    return ret;
+}
+
+void free_ste_list(struct ste* list) {
+    struct ste* cur_node = list;
+
+    while (cur_node != NULL) {
+        struct ste* prev = cur_node->prev;
+        free(cur_node);
+        cur_node = prev;
+    }
+
+    return;
+}
+
+struct decl* lookup_stack(struct id* arg_id) {
+    // empty stack
+    // printf("lineno : %d\n", read_line());
+    if (top == NULL || top->ste == NULL) return NULL;
+    if (arg_id == NULL) return NULL;
+	/*
+	 * This is for debugging
+	 */
+	// struct ste* test_node = top->ste; 
+	// while (test_node != NULL) {
+	// 	printf("cur node's name is : %s\n", test_node->name->name);
+	// 	test_node = test_node->prev;
+	// }
+
+    struct ste* cur_node = top->ste;
+    // Top Down searching
+    while (cur_node != NULL && (top->prev != NULL ? (cur_node != top->prev->ste) : 1)) {
+        if (cur_node->name == arg_id) {
+            return cur_node->decl;
+        }
+        cur_node = cur_node->prev;
+    }
+    
+    return NULL;
+}
+
+struct decl* lookup_whole(struct id* arg_id) {
+    if (arg_id == NULL) return NULL;
+    struct ste* cur_node = top->ste;
+
+    while (cur_node != NULL) {
+        if(cur_node->name == arg_id) {
+            return cur_node->decl;
+        }
+        cur_node = cur_node->prev;
+    }
+
+    return NULL;
+}
+
+struct decl* lookup_func() {
+    // // empty stack
+    // struct ste* cur_node = top->ste;
+    // struct ste* ret = NULL;
+
+    // while (cur_node != NULL) {
+    //     if (cur_node->decl->declclass == 2) {
+    //         return cur_node->decl;
+    //     }
+    //     cur_node = cur_node->prev;
+    // }
+    
+    // return NULL;
+    return cur_func;
+}
+
+struct decl* lookup_struct(struct id* name) {
+    if (name == NULL)
+        return NULL;
+    struct ste* cur_node = top->ste;
+
+    while (cur_node != NULL) {
+        if (cur_node->decl == inttype) 
+            break;
+        cur_node = cur_node->prev;
+    }
+
+    // search only below int type
+    while (cur_node != NULL) {
+        if (cur_node->name == name) 
+            return cur_node->decl;
+        cur_node = cur_node->prev;
+    }
+
+    return NULL;
+}
---
+++ project4/skeleton_proj4/subc.h
@@ -0,0 +1,114 @@
+/******************************************************
+ * File Name   : subc.h
+ * Description
+ *    This is a header file for the subc program.
+ ******************************************************/
+
+#ifndef __SUBC_H__
+#define __SUBC_H__
+
+#include <stdio.h>
+#include <strings.h>
+
+/* structure for ID */
+typedef struct id {
+      char *name;
+      int lextype;
+} id;
+
+/* For semantic analysis */
+struct ste {
+    struct id	*name;
+    struct decl	*decl;
+    struct ste	*prev;
+};
+
+/* structure for decl */
+struct decl {
+    int declclass;	/* DECL Class: VAR, CONST, FUNC, TYPE		*/
+    struct decl *type;		/* VAR, CONST: pointer to its type decl		*/
+    int value;		/* CONST: value of integer const		*/
+    char *char_value;	/* CONST: value of char const			*/
+    struct ste *formals;	/* FUNC: ptr to formals list			*/
+    struct decl *returntype;	/* FUNC: ptr to return TYPE decl		*/
+    int defined;        /* FUNC: check of function definition: not declared, declared, defined       */
+    int typeclass;	/* TYPE: type class: int, char, array, ptr, struct, void		*/
+    struct decl *elementvar;	/* TYPE (array): ptr to element VAR decl	*/
+    int num_index;	/* TYPE (array): number of elements		*/
+    struct ste *fieldlist;	/* TYPE (struct): ptr to field list		*/
+    struct decl *ptrto;		/* TYPE (pointer): type of the pointer		*/
+    int ptrcoef;	/* ALL: for check temp decls                    */
+    int offset;		/* ALL: local offset                            */
+    int isglobal;	/* ALL: isglobal                                */
+    int size;		/* ALL: size in bytes				*/
+    struct decl *origin;        /* ALL: origin for clone decl                   */
+    struct ste **scope;	/* VAR: scope when VAR declared			*/
+    struct decl *next;		/* For list_of_variables declarations		*/
+};                /* Or parameter check of function call		*/
+
+/* For scope stack */
+struct stack_node {
+    struct ste* ste;
+    struct stack_node* next;
+    struct stack_node* prev;
+
+};
+struct stack_node* top; 
+struct stack_node* bottom;
+
+void push_scope();
+void insert(struct id* arg_id, struct decl* arg_decl);
+struct ste* pop_scope();
+void free_ste_list(struct ste* list);
+struct decl* lookup_stack(struct id* arg_id);
+struct decl* lookup_whole(struct id* arg_id);
+struct decl* lookup_func();
+struct decl* lookup_struct(struct id* name);
+
+/* For hash table */
+unsigned hash(char *name);
+struct id *enter(int lextype, char *name, int length);
+struct id *lookup(char *name);
+
+int read_line();
+
+/* For semantic analysis */
+struct decl* maketypedecl(int type);
+struct decl* makevardecl(struct decl* typedecl);
+struct decl* makepointerdecl(int isPtr, struct decl* typedecl);
+struct decl* makearraydecl(struct decl* const_decl, struct decl* var_decl);
+struct decl* makeconstdecl(struct decl* type_decl) ;
+struct decl* makeintconstdecl(int int_const);
+struct decl* makecharconstdecl(char* char_const);
+struct decl* clonedecl(struct decl*);
+void check_struct_isdefined(struct id* arg_id);
+struct decl* makestructdecl(struct ste* arg_list);
+struct decl* findcurrentdecl(struct id* arg_id);
+void check_is_struct_type(struct decl* arg_decl);
+struct decl* makeprocdecl();
+struct decl* addpointer(struct decl* arg_decl);
+void check_incable(struct decl* arg_decl);
+struct decl* reference_ptr(struct decl * arg_decl);
+struct decl* reference_array(struct decl* ptr_decl, struct decl* const_decl);
+int check_compatibility(struct decl* arg1, struct decl* arg2, int enable);
+struct decl* reference_struct(struct decl* struct_name, struct id* member);
+void add_formals(struct decl* procdecl, struct ste* formals);
+void check_return_type_compatibility(struct decl* type1, struct decl* type2);
+void declare(struct id* arg_id, struct decl* arg_decl);
+void declare_struct(struct id* arg_id, struct decl* arg_decl);
+
+/* type decl */
+struct decl* inttype;
+struct decl* chartype;
+struct decl* voidtype;
+struct id* returnid;
+// for return type checking
+struct decl* cur_func;
+
+/* file name , readline */
+int read_line();
+char* get_filename();
+void print_error(const char*);
+
+#endif
+
